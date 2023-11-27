02398ca19a54d4a85127d39a827094c6aafdaef8
Master-Bobba <bayvazov94@gmail.com>
2023-11-27 11:48:51 +0000

    some small changes

--- src/main/java/com/example/service/ArtistService.java
+++ src/main/java/com/example/service/ArtistService.java
@@ -1,8 +1,10 @@
 package com.example.service;
 
 import com.example.model.Artist;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 public interface ArtistService {
 
--- src/main/java/com/example/service/ArtistServiceImp.java
+++ src/main/java/com/example/service/ArtistServiceImp.java
@@ -3,14 +3,15 @@ package com.example.service;
 import com.example.model.Artist;
 import com.example.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ArtistServiceImp implements ArtistService{
 
     private ArtistRepository artistRepository;
-
 
     @Autowired
     public ArtistServiceImp(ArtistRepository artistRepository) {

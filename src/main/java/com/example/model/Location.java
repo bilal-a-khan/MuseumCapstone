fbe12ced5173f890d8cc5643e5f152dfa0278e17
bilal-a-khan <142792067+bilal-a-khan@users.noreply.github.com>
2023-11-27 11:07:24 +0000

    Populate Museum and Location model classes

--- src/main/java/com/example/model/Location.java
+++ src/main/java/com/example/model/Location.java
@@ -1,4 +1,24 @@
 package com.example.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Location {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String city;
+    private String country;
+
+    @OneToOne(mappedBy = "location")
+    private Museum museum;
 }

4afa2ae3fa4ef5f263fc0fa0fc416e46e2e70395
bilal-a-khan <142792067+bilal-a-khan@users.noreply.github.com>
2023-12-01 08:58:52 +0000

    remove david2.0 from data.sql

--- src/main/java/com/example/MuseumCapstoneApplication.java
+++ src/main/java/com/example/MuseumCapstoneApplication.java
@@ -1,20 +1,13 @@
 package com.example;
 
-//import com.example.utils.Populator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MuseumCapstoneApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MuseumCapstoneApplication.class, args);
-
-//        ApplicationContext context = SpringApplication.run(MuseumCapstoneApplication.class, args);
-
-//        Populator populator = context.getBean(Populator.class);
-//        populator.populate();
 
     }
 
